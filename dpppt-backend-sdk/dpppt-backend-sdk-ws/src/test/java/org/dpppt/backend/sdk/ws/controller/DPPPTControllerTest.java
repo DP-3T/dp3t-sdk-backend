55972dc5fabd1b1ec803d5475cd74def49ddd3c2
Patrick Amrein <amrein@ubique.ch>
2020-04-22 08:20:35 +0200

    add more tests

--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/test/java/org/dpppt/backend/sdk/ws/controller/DPPPTControllerTest.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/test/java/org/dpppt/backend/sdk/ws/controller/DPPPTControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.net.URL;
 import java.util.Base64;
 
 import org.dpppt.backend.sdk.model.ExposeeAuthData;
@@ -19,12 +20,9 @@ import org.dpppt.backend.sdk.model.ExposeeRequest;
 
 @SpringBootTest(properties =
 {
-    "ws.app.jwt.publickey=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArLd5ATQ/aW1aUaKGd8s7H8ixVuUBH0FBOnVj38AJy78goBrzZnHOtnwTyoerytviOEvHxquQHXm36rB5Pv0ZLvjR59deU9jOr9kCUYpllDOGGzMNO6WkZbkfWNvulBJPOIcRm/WLqLX33mQkm7yHtIQk7M5XHTrkcgdpSAMGlEksq0n3/cTmKMhN9fng4Heh8SiXOtFzBUBo5cLI372VQiMzQPnQkR6ncSG0R6zzJUh+ZP2uP1z+fElHcZrE0d05KE7Y3Y9SykIQfYdyA5majGEFOd4GedVPoYVIgttCJQbiWGL3kCirFAhaRqGYIOrk+Fk8u67/x9Iinasuc18qAQIDAQAB"
+    "ws.app.jwt.publickey=classpath://correctPub.pem"
  })
 public class DPPPTControllerTest extends BaseControllerTest {
-
-    private final String jwtToken = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI1LVJxcVRUWW9tZnBWejA2VlJFT2ZyYmNxYTVXdTJkX1g4MnZfWlRNLVZVIn0.eyJqdGkiOiI4ZmE5MWRlMi03YmYwLTRhNmYtOWIzZC1hNzdiZDM3ZDdiMTMiLCJleHAiOjE1ODczMTYzMTgsIm5iZiI6MCwiaWF0IjoxNTg3MzE2MDE4LCJpc3MiOiJodHRwczovL2lkZW50aXR5LXIuYml0LmFkbWluLmNoL3JlYWxtcy9iYWctcHRzIiwic3ViIjoiMWVmYTliZWYtOWU5ZC00MjNjLTkxMjctZmQwYjAxNWQxOTY2IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoicHRhLWFwcC1iYWNrZW5kIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiZGVjYzQ3Y2YtMmUyZi00NjZlLThiNTAtZmQ4MTAzZmQ4ZDNhIiwiYWNyIjoiMSIsInNjb3BlIjoiZXhwb3NlZCIsIm9uc2V0IjoiMjAyMC0wNS0yNSJ9.J5beGE6GjgRWEZfwzB9_G6X1uTZcZdm7Mkng8od5Fr3UPT4BbkKgPbpGRscouiAPBjOlZDCs3rcT_qiioX5wAZ0UjqLTe370K53vb1I_f4nQKfTMBYfzvdpS5i5V64LoKbXHpF7PLsGSiox6dA8g5Ssqf5uoTHz1_NY-6GvVq-LmFozV6_1zzYkBVZCLVh0gsqcG9EH2peuhEt9akv_Jmc1Ls0lZQeU1szeRmsk44mg8_FbG33yB3F0azhs0pfEuuYCzGbAqdFCU2RDnRCOXXr7o8Z_klrKE6NArWgbHbk8CE0a-3UwEdi6zw0xm1VNwbnMtjxVcyxECw7V2bSNu9A";
-
     @Test
     public void testHello() throws Exception {
         MockHttpServletResponse response = mockMvc.perform(get("/v1"))
@@ -46,6 +44,11 @@ public class DPPPTControllerTest extends BaseControllerTest {
                                                             .header("User-Agent", "MockMVC")
                                                             .content(json(exposeeRequest)))
                 .andExpect(status().is2xxSuccessful()).andReturn().getResponse();
+        response = mockMvc.perform(post("/v1/exposed")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + wrongToken)
+                                .header("User-Agent", "MockMVC")
+                                .content(json(exposeeRequest)))
+                .andExpect(status().is4xxClientError()).andReturn().getResponse();
     }
-
 }
