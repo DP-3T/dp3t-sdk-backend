bd13a5cca9e1d6259aea3f0854c739efc5537d62
martinalig <martinalig0@gmail.com>
2020-04-23 15:22:18 +0200

    Check Version Fix.

--- dpppt-backend-sdk/dpppt-backend-sdk-data/src/main/resources/db/migration/pgsql/afterMigrate.sql
+++ dpppt-backend-sdk/dpppt-backend-sdk-data/src/main/resources/db/migration/pgsql/afterMigrate.sql
@@ -1,12 +1,22 @@
-create or replace procedure reassign_objects_ownership() LANGUAGE 'plpgsql'
-as $BODY$
+CREATE FUNCTION reassign_membership() RETURNS void AS $$
 BEGIN
-	IF (SELECT current_setting('server_version_num')::int >= 110000) and EXISTS(SELECT FROM pg_catalog.pg_roles where rolname = current_user || '_role_full') THEN
-		execute format('reassign owned by %s to %s_role_full', user, current_database()); 
+	IF EXISTS(SELECT current_setting('server_version_num')::int >= 110000) THEN
+
+		create or replace procedure reassign_objects_ownership() LANGUAGE 'plpgsql'
+		as $BODY$
+		BEGIN
+			IF EXISTS(SELECT FROM pg_catalog.pg_roles where rolname = current_user || '_role_full') THEN
+				execute format('reassign owned by %s to %s_role_full', user, current_database()); 
+			END IF;
+		END $BODY$;
+
+
+		call reassign_objects_ownership();
+
+		drop procedure reassign_objects_ownership();
+
 	END IF;
-END $BODY$;
-
-
-call reassign_objects_ownership();
-
-drop procedure reassign_objects_ownership();
+END;
+$$ LANGUAGE plpgsql;
+select reassign_membership();
+DROP FUNCTION reassign_membership;
