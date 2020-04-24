45819700054b6ec47b8deb48559b90480c9b1305
Ciro Cardone <cardone.ciro@gmail.com>
2020-04-24 15:10:58 +0200

    fix getMaxExposedIdForDay and add test with Postgres database (#35)
    * fix getMaxExposedIdForDay and add test with Postgres database
    
    * fix after develop merge
    
    Co-authored-by: Ciro Cardone <c.cardone@widegroup.ch>
    Co-authored-by: Tobias Bachmann <bachmann@ubique.ch>

--- dpppt-backend-sdk/dpppt-backend-sdk-data/src/main/java/org/dpppt/backend/sdk/data/DPPPTDataService.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-data/src/main/java/org/dpppt/backend/sdk/data/DPPPTDataService.java
@@ -16,25 +16,25 @@ public interface DPPPTDataService {
 	/**
 	 * Upserts the given exposee
 	 * 
-	 * @param exposee
-	 * @param appSource
+	 * @param exposee the exposee to upsert
+	 * @param appSource the app name
 	 */
 	void upsertExposee(Exposee exposee, String appSource);
 
 	/**
-	 * returns all exposees for the given day [day: 00:00, day+1: 00:00)
+	 * Returns all exposees for the given day [day: 00:00, day+1: 00:00] ordered by id
 	 * 
-	 * @param day
-	 * @return
+	 * @param day the day for which exposees are requested
+	 * @return exposee list
 	 */
 	List<Exposee> getSortedExposedForDay(DateTime day);
 
 	/**
 	 * Returns the maximum id of the stored exposed entries for the given day date
 	 * 
-	 * @param day
+	 * @param day the day for which id is required
 	 * 
-	 * @return
+	 * @return the max id or 0
 	 */
 	Integer getMaxExposedIdForDay(DateTime day);
 
