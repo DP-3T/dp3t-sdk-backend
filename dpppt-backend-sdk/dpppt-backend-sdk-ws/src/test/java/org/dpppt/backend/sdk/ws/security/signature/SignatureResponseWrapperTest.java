ee1ef45ae1d83b5796f344936da5cf84d5225003
Patrick Amrein <amrein@ubique.ch>
2020-04-30 17:03:36 +0200

    add more tests

--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/test/java/org/dpppt/backend/sdk/ws/security/signature/SignatureResponseWrapperTest.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/test/java/org/dpppt/backend/sdk/ws/security/signature/SignatureResponseWrapperTest.java
@@ -1,24 +1,29 @@
 package org.dpppt.backend.sdk.ws.security.signature;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SignatureException;
 import org.apache.commons.codec.binary.Hex;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.security.KeyPair;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Collections;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.Assert.assertTrue;
+
 
 /**
  * @author bachmann
@@ -35,7 +40,9 @@ public class SignatureResponseWrapperTest {
         response = new MockHttpServletResponse();
         KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.ES256);
         KeyPair wrongKey = Keys.keyPairFor(SignatureAlgorithm.ES256);
-        SignatureResponseWrapper signatureResponseWrapper = new SignatureResponseWrapper(response, keyPair, 21, Collections.emptyList());
+        List<String> protectedHeaders = new ArrayList<String>();
+        protectedHeaders.add("X-BATCH-RELEASE-TIME");
+        SignatureResponseWrapper signatureResponseWrapper = new SignatureResponseWrapper(response, keyPair, 21, protectedHeaders);
         signatureResponseWrapper.getOutputStream().print("TEST");
         signatureResponseWrapper.flushBuffer();
         String digest = response.getHeader("Digest");
@@ -43,11 +50,68 @@ public class SignatureResponseWrapperTest {
         assertNotNull(digest);
         String expected = "sha-256=" + Hex.encodeHexString(MessageDigest.getInstance("SHA-256").digest("TEST".getBytes()));
         assertEquals(expected, digest);
+        
+    }
+
+    @Test
+    public void testSignatureViaOutput() throws IOException, NoSuchAlgorithmException {
+        response = new MockHttpServletResponse();
+        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.ES256);
+        KeyPair wrongKey = Keys.keyPairFor(SignatureAlgorithm.ES256);
+        List<String> protectedHeaders = new ArrayList<String>();
+        protectedHeaders.add("X-BATCH-RELEASE-TIME");
+        SignatureResponseWrapper signatureResponseWrapper = new SignatureResponseWrapper(response, keyPair, 21, protectedHeaders);
+        signatureResponseWrapper.getOutputStream().print("TEST");
+        OutputStream stream = OutputStream.nullOutputStream();
+        signatureResponseWrapper.outputData(stream);
+        String digest = response.getHeader("Digest");
+        String rawJWT = response.getHeader("Signature");
+        assertNotNull(digest);
+        String expected = "sha-256=" + Hex.encodeHexString(MessageDigest.getInstance("SHA-256").digest("TEST".getBytes()));
+        assertEquals(expected, digest);
+    }
+
+    @Test
+    public void testBatchReleaseTime() throws IOException, NoSuchAlgorithmException{
+        response = new MockHttpServletResponse();
+        response.setHeader("X-BATCH-RELEASE-TIME", Long.toString(OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC).toInstant().toEpochMilli()));
+        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.ES256);
+        KeyPair wrongKey = Keys.keyPairFor(SignatureAlgorithm.ES256);
+        List<String> protectedHeaders = new ArrayList<String>();
+        protectedHeaders.add("X-BATCH-RELEASE-TIME");
+        SignatureResponseWrapper signatureResponseWrapper = new SignatureResponseWrapper(response, keyPair, 21, protectedHeaders);
+        signatureResponseWrapper.getOutputStream().print("TEST");
+        signatureResponseWrapper.flushBuffer();
+        String digest = response.getHeader("Digest");
+        String rawJWT = response.getHeader("Signature");
         JwtParser jwtParser = Jwts.parserBuilder().setSigningKey(keyPair.getPublic()).build();
         Jwt jwt = jwtParser.parse(rawJWT);
-        assertThrows(SignatureException.class, () -> {
-            Jwts.parserBuilder().setSigningKey(wrongKey.getPublic()).build().parse(rawJWT);
-        });
+    
+        assertTrue(response.containsHeader("X-BATCH-RELEASE-TIME"));
+        Claims claims = (Claims)jwt.getBody();
+        assertTrue(claims.containsKey("batch-release-time"));
+    }
+
+    @Test
+    public void testBatchReleaseTimeWithOutputStream() throws IOException, NoSuchAlgorithmException{
+        response = new MockHttpServletResponse();
+        response.setHeader("X-BATCH-RELEASE-TIME", Long.toString(OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC).toInstant().toEpochMilli()));
+        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.ES256);
+        KeyPair wrongKey = Keys.keyPairFor(SignatureAlgorithm.ES256);
+        List<String> protectedHeaders = new ArrayList<String>();
+        protectedHeaders.add("X-BATCH-RELEASE-TIME");
+        SignatureResponseWrapper signatureResponseWrapper = new SignatureResponseWrapper(response, keyPair, 21, protectedHeaders);
+        signatureResponseWrapper.getOutputStream().print("TEST");
+        OutputStream stream = OutputStream.nullOutputStream();
+        signatureResponseWrapper.outputData(stream);
+        String digest = response.getHeader("Digest");
+        String rawJWT = response.getHeader("Signature");
+        JwtParser jwtParser = Jwts.parserBuilder().setSigningKey(keyPair.getPublic()).build();
+        Jwt jwt = jwtParser.parse(rawJWT);
+    
+        assertTrue(response.containsHeader("X-BATCH-RELEASE-TIME"));
+        Claims claims = (Claims)jwt.getBody();
+        assertTrue(claims.containsKey("batch-release-time"));
     }
 
 }
