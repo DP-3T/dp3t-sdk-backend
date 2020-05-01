9cc99f0b073fd910dec8cab783dc4ce35ec48d28
Patrick Amrein <amrein@ubique.ch>
2020-05-01 13:58:29 +0200

    add both generators

--- GenerateKeyPairEC.java
+++ GenerateKeyPairEC.java
@@ -1,5 +1,6 @@
 /**
  * This file only serves as an example on how to get keys in the right encoding. 
+ * In order to use it you need BouncyCastle on the classpath!
  * 
  * DO NOT USE THEM IN PRODUCTION UNLESS THE KEYSPECS ARE OK FOR YOU
  */
@@ -12,13 +13,18 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Base64;
+import java.security.spec.ECGenParameterSpec;
 
 import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-public class GenerateKeyPair{
+public class GenerateKeyPairEC{
     public static void main(String[] args) throws Exception {
+		Security.addProvider(new BouncyCastleProvider());
 		Security.setProperty("crypto.policy", "unlimited");
-		KeyPairGenerator generator =  KeyPairGenerator.getInstance("RSA");
+		KeyPairGenerator generator =  KeyPairGenerator.getInstance("ECDSA", "BC");
+		ECGenParameterSpec spec = new ECGenParameterSpec("secp256r1");
+		generator.initialize(spec);
 		KeyPair pair = generator.genKeyPair();
 		PrivateKey privateKey = pair.getPrivate();
 		PublicKey publicKey = pair.getPublic();
