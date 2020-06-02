02fed89be373a9cff9a2b2ffec346c43e076098c
Patrick Amrein <amrein@ubique.ch>
2020-06-02 17:54:18 +0200

    add published until header and cache control according to RFC

--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/controller/GaenController.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/controller/GaenController.java
@@ -216,11 +216,12 @@ public class GaenController {
 
 		ProtoSignatureWrapper payload = gaenSigner.getPayload(exposedKeys);
 		String etag = Base64.getEncoder().encodeToString(payload.getHash());
-		if(request.checkNotModified(etag)) {
-			return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
+		if (request.checkNotModified(etag)) {
+			return ResponseEntity.status(HttpStatus.NOT_MODIFIED)
+					.header("X-PUBLISHED-UNTIL", Long.toString(publishedUntil))
+					.cacheControl(CacheControl.maxAge(exposedListCacheContol)).build();
 		}
-		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(exposedListCacheContol))
-				.eTag(etag)
+		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(exposedListCacheContol)).eTag(etag)
 				.header("X-PUBLISHED-UNTIL", Long.toString(publishedUntil)).body(payload.getZip());
 	}
 
