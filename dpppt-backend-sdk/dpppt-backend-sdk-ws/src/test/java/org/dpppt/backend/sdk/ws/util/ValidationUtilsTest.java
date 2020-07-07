eee3ec875f11540be08af836e5ba2e51e13fc6a8
Patrick Amrein <amrein@ubique.ch>
2020-07-07 21:17:10 +0200

    use assertequals

--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/test/java/org/dpppt/backend/sdk/ws/util/ValidationUtilsTest.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/test/java/org/dpppt/backend/sdk/ws/util/ValidationUtilsTest.java
@@ -1,7 +1,6 @@
 package org.dpppt.backend.sdk.ws.util;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -15,14 +14,14 @@ public class ValidationUtilsTest {
     public void testOnlyMidnightIsValid() throws Exception {
         var validationUtils = new ValidationUtils(16, Duration.ofDays(14), Duration.ofHours(2).toMillis());
         var midnight = LocalDate.now(ZoneOffset.UTC).atStartOfDay(ZoneOffset.UTC);
-        assertTrue(validationUtils.isValidKeyDate(midnight.toInstant().toEpochMilli()));
-        assertFalse(validationUtils.isValidKeyDate(midnight.minusSeconds(1).toInstant().toEpochMilli()));
-        assertFalse(validationUtils.isValidKeyDate(midnight.minusMinutes(1).toInstant().toEpochMilli()));
-        assertFalse(validationUtils.isValidKeyDate(midnight.minusHours(1).toInstant().toEpochMilli()));
-        assertFalse(validationUtils.isValidKeyDate(midnight.plusSeconds(1).toInstant().toEpochMilli()));
-        assertFalse(validationUtils.isValidKeyDate(midnight.plusMinutes(1).toInstant().toEpochMilli()));
-        assertFalse(validationUtils.isValidKeyDate(midnight.plusHours(1).toInstant().toEpochMilli()));
+        assertEquals(true, validationUtils.isValidKeyDate(midnight.toInstant().toEpochMilli()));
+        assertEquals(false, validationUtils.isValidKeyDate(midnight.minusSeconds(1).toInstant().toEpochMilli()));
+        assertEquals(false, validationUtils.isValidKeyDate(midnight.minusMinutes(1).toInstant().toEpochMilli()));
+        assertEquals(false, validationUtils.isValidKeyDate(midnight.minusHours(1).toInstant().toEpochMilli()));
+        assertEquals(false, validationUtils.isValidKeyDate(midnight.plusSeconds(1).toInstant().toEpochMilli()));
+        assertEquals(false, validationUtils.isValidKeyDate(midnight.plusMinutes(1).toInstant().toEpochMilli()));
+        assertEquals(false, validationUtils.isValidKeyDate(midnight.plusHours(1).toInstant().toEpochMilli()));
         
-        assertTrue(LocalTime.MIDNIGHT.equals(midnight.toLocalTime()));
+        assertEquals(true, LocalTime.MIDNIGHT.equals(midnight.toLocalTime()));
     }
 }
