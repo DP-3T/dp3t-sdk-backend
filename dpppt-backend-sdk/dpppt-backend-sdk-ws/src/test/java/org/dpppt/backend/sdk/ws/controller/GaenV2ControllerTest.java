227f6c38d505d00ce5b4fca92dce28b9ca10df61
Simon Rösch <roesch@ubique.ch>
2020-10-28 22:28:05 +0100

    add test to verify upload and release of key with shortened rolling period

--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/test/java/org/dpppt/backend/sdk/ws/controller/GaenV2ControllerTest.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/test/java/org/dpppt/backend/sdk/ws/controller/GaenV2ControllerTest.java
@@ -214,4 +214,129 @@ public class GaenV2ControllerTest extends BaseControllerTest {
               .getResponse();
     }
   }
+
+  @Test
+  public void testUploadWithShortenedRollingPeriodWithV1Upload() throws Exception {
+    testUploadWithShortenedRollingPeriod(false);
+  }
+
+  @Test
+  public void testUploadWithShortenedRollingPeriodWithV2Upload() throws Exception {
+    testUploadWithShortenedRollingPeriod(true);
+  }
+
+  private void testUploadWithShortenedRollingPeriod(boolean useV2Upload) throws Exception {
+    // set keyReleasTime to 14:10 UTC
+    var keyReleaseTime = UTCInstant.now().atStartOfDay().plusHours(14).plusMinutes(10);
+    List<GaenKey> keys = new ArrayList<>();
+    for (int i = 0; i < 30; i++) {
+      var tmpKey = new GaenKey();
+      tmpKey.setRollingStartNumber(
+          (int) keyReleaseTime.atStartOfDay().minusDays(i).get10MinutesSince1970());
+      var keyData = String.format("testKey32Bytes%02d", i);
+      tmpKey.setKeyData(Base64.getEncoder().encodeToString(keyData.getBytes("UTF-8")));
+      tmpKey.setRollingPeriod(144);
+      tmpKey.setFake(0);
+      tmpKey.setTransmissionRiskLevel(0);
+      keys.add(tmpKey);
+    }
+    // set shortened rolling period for first key
+    keys.get(0).setRollingPeriod(85);
+
+    Object uploadPayload;
+    if (useV2Upload) {
+      GaenV2UploadKeysRequest exposeeRequest = new GaenV2UploadKeysRequest();
+      exposeeRequest.setGaenKeys(keys);
+      uploadPayload = exposeeRequest;
+    } else {
+      GaenRequest exposeeRequest = new GaenRequest();
+      exposeeRequest.setGaenKeys(keys);
+      exposeeRequest.setDelayedKeyDate((int) keyReleaseTime.atStartOfDay().get10MinutesSince1970());
+      uploadPayload = exposeeRequest;
+    }
+
+    String token = createToken(UTCInstant.now().plusMinutes(5));
+
+    String keyBundleTag;
+
+    try (var timeLock =
+        UTCInstant.setClock(Clock.fixed(keyReleaseTime.getInstant(), ZoneOffset.UTC))) {
+      MvcResult responseAsync =
+          mockMvc
+              .perform(
+                  post(useV2Upload ? "/v2/gaen/exposed" : "/v1/gaen/exposed")
+                      .contentType(MediaType.APPLICATION_JSON)
+                      .header("Authorization", "Bearer " + token)
+                      .header("User-Agent", androidUserAgent)
+                      .content(json(uploadPayload)))
+              .andExpect(request().asyncStarted())
+              .andReturn();
+      mockMvc.perform(asyncDispatch(responseAsync)).andExpect(status().isOk());
+
+      MockHttpServletResponse response =
+          mockMvc
+              .perform(get("/v2/gaen/exposed").header("User-Agent", androidUserAgent))
+              .andExpect(status().is(204))
+              .andReturn()
+              .getResponse();
+
+      keyBundleTag = response.getHeader("x-key-bundle-tag");
+    }
+
+    // at 16:00 UTC we expect all the keys but the one from today, because this one might still
+    // be accepted by client apps
+    Clock fourPMToday =
+        Clock.fixed(
+            keyReleaseTime.atStartOfDay().plusHours(16).plusSeconds(0).getInstant(),
+            ZoneOffset.UTC);
+
+    try (var timeLock = UTCInstant.setClock(fourPMToday)) {
+      MockHttpServletResponse response =
+          mockMvc
+              .perform(
+                  get("/v2/gaen/exposed?lastKeyBundleTag=" + keyBundleTag)
+                      .header("User-Agent", androidUserAgent))
+              .andExpect(status().isOk())
+              .andReturn()
+              .getResponse();
+      verifyZipResponse(response, 14, 144);
+      keyBundleTag = response.getHeader("x-key-bundle-tag");
+    }
+
+    // at 18:00 UTC we expect the one key from today, because this one was delayed
+    Clock sixPMToday =
+        Clock.fixed(
+            keyReleaseTime.atStartOfDay().plusHours(18).plusSeconds(0).getInstant(),
+            ZoneOffset.UTC);
+
+    try (var timeLock = UTCInstant.setClock(sixPMToday)) {
+      MockHttpServletResponse response =
+          mockMvc
+              .perform(
+                  get("/v2/gaen/exposed?lastKeyBundleTag=" + keyBundleTag)
+                      .header("User-Agent", androidUserAgent))
+              .andExpect(status().isOk())
+              .andReturn()
+              .getResponse();
+      verifyZipResponse(response, 1, 85);
+      keyBundleTag = response.getHeader("x-key-bundle-tag");
+    }
+
+    // at 20:00 UTC we expect no further keys
+    Clock eightPMToday =
+        Clock.fixed(
+            keyReleaseTime.atStartOfDay().plusHours(20).plusSeconds(0).getInstant(),
+            ZoneOffset.UTC);
+
+    try (var timeLock = UTCInstant.setClock(eightPMToday)) {
+      MockHttpServletResponse response =
+          mockMvc
+              .perform(
+                  get("/v2/gaen/exposed?lastKeyBundleTag=" + keyBundleTag)
+                      .header("User-Agent", androidUserAgent))
+              .andExpect(status().is(204))
+              .andReturn()
+              .getResponse();
+    }
+  }
 }
