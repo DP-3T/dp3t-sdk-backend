c8573bcde2221204ce700d499b5b533548f1a76f
José Pereira <jop@di.uminho.pt>
2020-10-28 12:30:08 +0000

    Use failTimeout instead of failFast for Hikari >= 3.0.0
    initializationFailFast has been removed from Hikari >= 3.0.0 and replaced
    with initializationFailTimeout. See https://github.com/brettwooldridge/HikariCP#infrequently-used

--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/config/WSProdConfig.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/config/WSProdConfig.java
@@ -50,8 +50,8 @@ public class WSProdConfig extends WSBaseConfig {
   @Value("${datasource.driverClassName}")
   String dataSourceDriver;
 
-  @Value("${datasource.failFast}")
-  String dataSourceFailFast;
+  @Value("${datasource.failTimeout}")
+  String dataSourceFailTimeout;
 
   @Value("${datasource.maximumPoolSize}")
   String dataSourceMaximumPoolSize;
@@ -84,6 +84,7 @@ public class WSProdConfig extends WSBaseConfig {
     config.setMaxLifetime(Integer.parseInt(dataSourceMaxLifetime));
     config.setIdleTimeout(Integer.parseInt(dataSourceIdleTimeout));
     config.setConnectionTimeout(Integer.parseInt(dataSourceConnectionTimeout));
+    config.setInitializationFailTimeout(Integer.parseInt(dataSourceFailTimeout));
     return new HikariDataSource(config);
   }
 
