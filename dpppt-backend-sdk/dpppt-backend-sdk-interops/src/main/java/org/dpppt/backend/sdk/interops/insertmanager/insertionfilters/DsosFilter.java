63900e8ff79ac84915f71b59ef90d0b6ddbcc141
Fabian Aggeler <aggeler@ubique.ch>
2021-03-16 18:06:43 +0100

    Fix DSOS range filter

--- dpppt-backend-sdk/dpppt-backend-sdk-interops/src/main/java/org/dpppt/backend/sdk/interops/insertmanager/insertionfilters/DsosFilter.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-interops/src/main/java/org/dpppt/backend/sdk/interops/insertmanager/insertionfilters/DsosFilter.java
@@ -26,9 +26,11 @@ public class DsosFilter implements InteropsKeyInsertionFilter {
       // dsos is already normalized
     } else if (dsos < 1986) { // onset range `n` days (n < 19). dsos in [n*100-14, n*100+14].
       final int nMax = 19;
-      final int n = dsos + nMax / 100;
+      // Detect n-day rage
+      final int n = (dsos + nMax) / 100;
       int endOfRange = n * 100;
-      int startOfRange = endOfRange - n;
+      // To get an n-day range subtract n-1 nights
+      int startOfRange = endOfRange - (n - 1);
       normalizedDsos = dsos - startOfRange;
     } else if (dsos < 2986) { // unknown onset. dsos in [1986, 2014]
       normalizedDsos = dsos - 2000;
