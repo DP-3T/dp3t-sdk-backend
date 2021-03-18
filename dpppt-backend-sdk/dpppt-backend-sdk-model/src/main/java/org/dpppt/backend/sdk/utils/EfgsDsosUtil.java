be97fffa8bf2e8441e478fc4c9b92fb198a8cb17
Fabian Aggeler <aggeler@ubique.ch>
2021-03-18 22:19:59 +0100

    Refactoring: Exctract claculateDsos to it's own class (EfgsDsosUtil) to make it testable

---
+++ dpppt-backend-sdk/dpppt-backend-sdk-model/src/main/java/org/dpppt/backend/sdk/utils/EfgsDsosUtil.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021 Ubique Innovation AG <https://www.ubique.ch>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+
+package org.dpppt.backend.sdk.utils;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import org.dpppt.backend.sdk.model.gaen.GaenKeyForInterops;
+import org.dpppt.backend.sdk.model.gaen.GaenUnit;
+
+public class EfgsDsosUtil {
+
+  public static int calculateDefaultDsosMapping(GaenKeyForInterops gaenKey) {
+    LocalDateTime rollingStartNumber =
+        UTCInstant.of(gaenKey.getRollingStartNumber(), GaenUnit.TenMinutes).getLocalDateTime();
+    LocalDateTime receivedAt = gaenKey.getReceivedAt().getLocalDateTime();
+    return (int) Duration.between(receivedAt, rollingStartNumber).toDays() + 2000;
+  }
+
+}
