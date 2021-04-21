4e9c4ad2d347bac2ea937291d3a663fed1652099
Felix Haller <haller@ubique.ch>
2021-04-21 10:08:51 +0200

    use p12 for signing certificate

--- dpppt-backend-sdk/dpppt-backend-sdk-interops/src/main/java/org/dpppt/backend/sdk/interops/syncer/efgs/signing/CryptoProvider.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-interops/src/main/java/org/dpppt/backend/sdk/interops/syncer/efgs/signing/CryptoProvider.java
@@ -12,57 +12,57 @@ package org.dpppt.backend.sdk.interops.syncer.efgs.signing;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UncheckedIOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.Security;
+import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMKeyPair;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import java.util.Base64;
 
 public class CryptoProvider {
 
+  // private final PrivateKey privateKey;
+  // private final X509Certificate publicKey;
+  private final KeyStore keyStore;
+  private final String alias;
   private final PrivateKey privateKey;
-  private final X509Certificate publicKey;
+  private final X509Certificate certificate;
 
-  /** Creates a CryptoProvider, using {@link BouncyCastleProvider}. */
-  public CryptoProvider(String signClientCert, String signClientCertPassword)
-      throws CertificateException {
-    privateKey = loadPrivateKey(signClientCertPassword);
-    publicKey = loadPublicKey(signClientCert);
-    Security.addProvider(new BouncyCastleProvider());
-  }
-
-  private static PrivateKey getPrivateKeyFromStream(InputStream privateKeyStream)
-      throws IOException {
-    PEMParser pemParser = new PEMParser(new InputStreamReader(privateKeyStream));
-    JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-    var parsedObject = pemParser.readObject();
-    if (parsedObject instanceof PEMKeyPair) {
-      return converter.getPrivateKey(((PEMKeyPair) parsedObject).getPrivateKeyInfo());
-    } else {
-      PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(parsedObject);
-      return converter.getPrivateKey(privateKeyInfo);
+  /** Creates a CryptoProvider */
+  public CryptoProvider(String signClientCertificate, String signClientCertificatePassword)
+      throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException,
+          UnrecoverableKeyException {
+    try (var byteInputStream =
+        new ByteArrayInputStream(Base64.getDecoder().decode(signClientCertificate))) {
+      this.keyStore = KeyStore.getInstance("PKCS12");
+      this.keyStore.load(byteInputStream, signClientCertificatePassword.toCharArray());
+      this.alias = keyStore.aliases().nextElement();
+      this.privateKey =
+          (PrivateKey) keyStore.getKey(this.alias, signClientCertificatePassword.toCharArray());
+      this.certificate = (X509Certificate) keyStore.getCertificate(alias);
     }
   }
 
-  private X509Certificate loadPublicKey(String signClientCert) throws CertificateException {
-    CertificateFactory cf = CertificateFactory.getInstance("X.509");
-    try (InputStream publicKeyStream = new ByteArrayInputStream(signClientCert.getBytes())) {
-      return (X509Certificate) cf.generateCertificate(publicKeyStream);
-    } catch (IOException e) {
-      throw new UncheckedIOException("Failed to load public key", e);
+  /** Creates a CryptoProvider */
+  public CryptoProvider(
+      String signClientCertificate, String signClientCertificatePassword, String alias)
+      throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException,
+          UnrecoverableKeyException {
+    try (var byteInputStream =
+        new ByteArrayInputStream(Base64.getDecoder().decode(signClientCertificate))) {
+      this.keyStore = KeyStore.getInstance("PKCS12");
+      this.keyStore.load(byteInputStream, signClientCertificatePassword.toCharArray());
+      this.alias = alias;
+      privateKey =
+          (PrivateKey) keyStore.getKey(this.alias, signClientCertificatePassword.toCharArray());
+      this.certificate = (X509Certificate) keyStore.getCertificate(alias);
     }
   }
 
   public X509Certificate getCertificate() {
-    return this.publicKey;
+    return this.certificate;
   }
 
   /**
@@ -71,15 +71,6 @@ public class CryptoProvider {
    * @return private key
    */
   public PrivateKey getPrivateKey() {
-    return privateKey;
-  }
-
-  private PrivateKey loadPrivateKey(String signClientCertPassword) {
-    try (InputStream privateKeyStream =
-        new ByteArrayInputStream(signClientCertPassword.getBytes())) {
-      return getPrivateKeyFromStream(privateKeyStream);
-    } catch (IOException e) {
-      throw new UncheckedIOException("Failed to load private key", e);
-    }
+    return this.privateKey;
   }
 }
